24499301ba19645507a5aaa0827e4a1125513586
Lucky Adike <luckyadike@gmail.com>
2015-05-02 09:42:15 -0700

    Layout improvements.

--- askative/App_Start/BundleConfig.cs
+++ askative/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace askative
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/site.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
